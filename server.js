cd8408f5dd5df97a852d6276a54a781f1143f17c
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-11-18 17:30:44 +0600

    Build Services Api

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+//Database Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iy3km.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+    try {
+        await client.connect();
+        console.log('db connected')
+        const database = client.db("health-care");
+        const servicesCollection = database.collection("services");
+        const bookingCollection =database.collection("booking");
+        //Get All Watches
+        app.get('/services',async(req,res)=>{
+                const cursor = await servicesCollection.find({});
+                const allServices = await cursor.toArray();
+                res.send(allServices);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Health-Care Server Running');
+});
+
+app.listen(port, () => {
+    console.log('Health-Care Server Running', port);
+})
