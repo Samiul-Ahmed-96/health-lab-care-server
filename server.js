5b3cb8a4c0c8e4bbc9c810334493ddb7f5e3912f
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-11-20 20:07:11 +0600

    Create Add Doctor post Api

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const app = express();
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
-const app = express();
 const port = process.env.PORT || 5000;
 
 //Middleware
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 //Database Connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iy3km.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -54,7 +56,22 @@ async function run() {
             const allBookings = await cursor.toArray();
             res.send(allBookings);
         })
-        
+        //Delete single item from Services Api
+        app.delete('/services/:id', async (req,res)=>{
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)};
+            const result = await servicesCollection.deleteOne(query);
+            console.log(result)
+            res.json(result)
+        })
+        //Delete single item from Bookings Api
+        app.delete('/bookings/:id', async (req,res)=>{
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)};
+            const result = await appoinmentsCollection.deleteOne(query);
+            console.log(result)
+            res.json(result)
+        })
         //Delete single item from appoinments Api
         app.delete('/bookings/:id', async (req,res)=>{
             const id = req.params.id;
@@ -62,6 +79,24 @@ async function run() {
             const result = await appoinmentsCollection.deleteOne(query);
             console.log(result)
             res.json(result)
+        })
+        //Add Doctor to Db
+        app.post('/doctors', async (req, res) => {
+            const name = req.body.name;
+            const email = req.body.email;
+            const designation =req.body.designation;
+            const pic = req.files.img;
+            const picData = pic.data;
+            const encodedPic = picData.toString('base64');
+            const imageBuffer = Buffer.from(encodedPic, 'base64');
+            const doctor = {
+                name,
+                email,
+                designation,
+                img: imageBuffer
+            }
+            const result = await doctorsCollection.insertOne(doctor);
+            res.json(result);
         })
         //Add Reviews Api
         app.post('/reviews',async(req,res)=>{
@@ -83,7 +118,26 @@ async function run() {
             const update = {$set : user}
             const result =await usersCollection.updateOne(filter,update,options);
             res.json(result)
-        })    
+        })
+         //Added Admin
+         app.put('/users/admin',async(req,res) =>{
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
+        //Check Admin
+        app.get('/users/:email' , async (req,res)=>{
+            const email = req.params.email; 
+            const query = {email : email};
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if(user?.role === 'admin'){
+                isAdmin = true;
+            }
+            res.json({admin : isAdmin})
+        })   
     }
     finally {
         // await client.close();
